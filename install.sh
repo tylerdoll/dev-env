ed72c6e633ecb79eec8587c81c5e67981ebe66ee
Tyler Doll <tylerdoll@mines.edu>
2019-02-06 04:45:32 +0000

    feat: add install script

---
+++ install.sh
@@ -0,0 +1,3 @@
+sudo git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp -r bash_completion.d .vimrc .bashrc .gitconfig ../
+mkdir ~/.vim/.backup ~/.vim/.swp/ ~/.vim/.undo
