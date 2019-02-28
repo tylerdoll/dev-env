09477ea1616fd319c7a667d73fd71828a0cba03f
tylerdoll <tdoll1997@gmail.com>
2019-02-28 10:12:27 -0700

    refac: change copys to symlinks

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
-sudo git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp -r bash_completion.d .vimrc .bashrc .gitconfig ../
+ln -s $(pwd)/bash_completion.d ~
+ln -s $(pwd)/.vimrc ~
+ln -s $(pwd)/.bashrc ~
+ln -s $(pwd)/.gitconfig ~
+
 mkdir ~/.vim/.backup ~/.vim/.swp/ ~/.vim/.undo
+sudo git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
