635fd956ba34577d4c30e66ba2a00c1c06ad6262
Tyler Doll <tyler@tylersmac.Home>
2020-03-06 16:16:35 -0700

    feat: add fzf install to install script

--- install.sh
+++ install.sh
@@ -6,3 +6,6 @@ ln -s $(pwd)/.gitconfig ~
 
 mkdir ~/.vim/.backup ~/.vim/.swp/ ~/.vim/.undo
 sudo git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install fzf
+scripts/fzf/install.sh
